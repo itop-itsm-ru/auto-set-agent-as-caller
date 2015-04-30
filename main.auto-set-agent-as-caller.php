dc98d54146e0b378d94df29b3b54326dd95cf021
Vladimir Kunin <v.b.kunin@gmail.com>
2015-04-30 15:57:30 +0300

    First commit

---
+++ main.auto-set-agent-as-caller.php
@@ -0,0 +1,91 @@
+<?php
+
+/**
+ * Module auto-set-agent-as-caller
+ *
+ * @author      Vladimir Kunin <v.b.kunin@gmail.com>
+ *              https://github.com/vbkunin
+ * @license     http://www.opensource.org/licenses/gpl-3.0.html LGPL
+ */
+
+
+class AutoSetAgentAsCaller implements iApplicationUIExtension
+{
+	public function OnDisplayProperties($oObject, WebPage $oPage, $bEditMode = false)
+	{
+		$bModuleEnabled = utils::GetConfig()->GetModuleSetting('auto-set-agent-as-caller', 'enabled');
+		if ($bModuleEnabled && $bEditMode && $oObject->IsNew())
+		{
+			$iOrgId = $this->GetUserOrgId();
+			$iCallerId = UserRights::GetContactId();
+			$oPage->add_ready_script(
+<<< EOF
+				if ($iOrgId && $iCallerId) {
+				  $("#field_2_caller_id").one("update", "select", function() {
+				    $("select#2_caller_id").val("$iCallerId").trigger("change");
+				  });
+					$("select#2_org_id").val("$iOrgId").trigger("change");
+				}
+EOF
+			);
+		}
+	}
+
+	public function OnDisplayRelations($oObject, WebPage $oPage, $bEditMode = false)
+	{
+	}
+
+	public function OnFormSubmit($oObject, $sFormPrefix = '')
+	{
+	}
+
+	public function OnFormCancel($sTempId)
+	{
+	}
+
+	public function EnumUsedAttributes($oObject)
+	{
+		return array();
+	}
+
+	public function GetIcon($oObject)
+	{
+		return '';
+	}
+
+	public function GetHilightClass($oObject)
+	{
+		// Possible return values are:
+		// HILIGHT_CLASS_CRITICAL, HILIGHT_CLASS_WARNING, HILIGHT_CLASS_OK, HILIGHT_CLASS_NONE
+		return HILIGHT_CLASS_NONE;
+	}
+
+	public function EnumAllowedActions(DBObjectSet $oSet)
+	{
+		// No action
+		return array();
+    }
+
+	///////////////////////////////////////////////////////////////////////////////////////////////////////
+	//
+	// Plug-ins specific functions
+	//
+	///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	protected function GetUserOrgId()
+	{
+		$iOrgId = 0;
+		$iContactId = UserRights::GetContactId();
+		$oContact = MetaModel::GetObject('Contact', $iContactId, false); // false => Can fail
+		if (is_object($oContact))
+		{
+			$oOrg = MetaModel::GetObject('Organization', $oContact->Get('org_id'), false); // false => can fail
+			if (is_object($oOrg))
+			{
+				$iOrgId = $oOrg->GetKey();
+			}
+		}
+		return $iOrgId;
+	}
+}
+?>
---
+++ module.auto-set-agent-as-caller.php
@@ -0,0 +1,74 @@
+<?php
+//
+// iTop module definition file
+//
+
+// Git commit info
+if (file_exists(dirname(__FILE__).'/.git/HEAD'))
+{
+	$sDate = date("d.m.y G:i", filemtime(dirname(__FILE__).'/.git/index'));
+	$sHead = trim(preg_replace('/ref:/', '', file_get_contents(dirname(__FILE__).'/.git/HEAD'), -1, $iCount));
+	if ($iCount === 1)
+	{
+		$sCommit = '(commit '.file_get_contents(dirname(__FILE__).'/.git/'.$sHead, false, null, 0, 7).' from '.$sDate.')';
+	}
+	elseif ($iCount === 0)
+	{
+		$sCommit = '(commit '.substr($sHead, 0, 7).' from '.$sDate.')';
+	}
+	else
+	{
+		$sCommit = '(commit info not found)';
+	}
+}
+else
+{
+	$sCommit = '';
+}
+
+
+SetupWebPage::AddModule(
+	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
+	'auto-set-agent-as-caller/1.0.0',
+	array(
+		// Identification
+		//
+		'label' => "Auto set this Agent as default Caller $sCommit",
+		'category' => 'business',
+
+		// Setup
+		//
+		'dependencies' => array(
+		),
+		'mandatory' => false,
+		'visible' => true,
+
+		// Components
+		//
+		'datamodel' => array(
+			'main.auto-set-agent-as-caller.php',
+		),
+		'webservice' => array(
+
+		),
+		'data.struct' => array(
+		),
+		'data.sample' => array(
+			// add your sample data XML files here,
+		),
+
+		// Documentation
+		//
+		'doc.manual_setup' => '', // hyperlink to manual setup documentation, if any
+		'doc.more_information' => '', // hyperlink to more information, if any
+
+		// Default settings
+		//
+		'settings' => array(
+			'enabled' => true,
+		),
+	)
+);
+
+
+?>
